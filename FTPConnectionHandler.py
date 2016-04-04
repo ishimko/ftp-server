bc1c8e0c65c7d2f135321cf1cf999a4e55b8ce22
Ivan <vana.shimko@gmail.com>
2016-04-04 23:50:18 +0300

    closing connection, log fixes

--- FTPConnectionHandler.py
+++ FTPConnectionHandler.py
@@ -11,7 +11,9 @@ class FTPThreadHandler(Thread):
         530: b'530 Not logged in.',
         230: b'230 User logged in, proceed.',
         200: b'200 Command okay.',
-        500: b'500 Syntax error, command unrecognized.'
+        500: b'500 Syntax error, command unrecognized.',
+        221: b'221 Service closing control connection.',
+        502: b'502 Command not implemented.'
     }
 
     def __init__(self, connection, root_dir, users):
@@ -22,6 +24,7 @@ class FTPThreadHandler(Thread):
         self.username = None
         self.passive_mode = False
         self.current_dir = root_dir
+        self.is_closed = False
 
     @staticmethod
     def get_command_name(command_text):
@@ -33,9 +36,10 @@ class FTPThreadHandler(Thread):
 
     def run(self):
         log('{a[0]}:{a[1]} connected'.format(a=self.address))
+        command_text = None
 
         self.client_socket.send(b'220 Welcome!\r\n')
-        while True:
+        while not self.is_closed:
             try:
                 command_text = self.client_socket.recv(FTPThreadHandler.BUFFER_SIZE)
                 if command_text:
@@ -57,20 +61,33 @@ class FTPThreadHandler(Thread):
 
     def USER(self, command_text):
         username = (command_text.split()[1]).decode('ascii')
+        log_message = 'username {}'.format(username)
         if username in self.users:
             self.username = username
-            log('{}: username accepted'.format(username))
+            log(log_message + ': accepted')
             self.send_answer(331)
         else:
-            log('{}: username not found'.format(username))
+            log(log_message + ': username not found')
             self.send_answer(530)
 
     def PASS(self, command_text):
         password = (command_text.split()[1]).decode('ascii')
-        log_mesage = 'user {}, password {}'.format(self.username, password)
+        log_message = 'user: {}, password: {}'.format(self.username, password)
         if self.users.get(self.username) == password:
-            log(log_mesage + ': access granted')
+            log(log_message + ': access granted')
             self.send_answer(230)
         else:
-            log(log_mesage + ': access denied')
+            log(log_message + ': access denied')
             self.send_answer(530)
+
+    def QUIT(self, _):
+        self.send_answer(221)
+        self.client_socket.close()
+        self.close()
+        log('{} disconnected'.format(self.username))
+
+    def NOOP(self, _):
+        self.send_answer(200)
+
+    def close(self):
+        self.is_closed = True
